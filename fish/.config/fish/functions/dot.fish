a4015afcaf0000fd9d9577bb34cdba711fb12a99
Cory G <cory.gantenbein@protonmail.com>
2019-06-03 15:26:51 -0500

    fish functions, $DOTSDIR usage

--- fish/.config/fish/functions/dot.fish
+++ fish/.config/fish/functions/dot.fish
@@ -1,4 +1,4 @@
 # Defined in - @ line 1
 function dot --description 'alias dot git --git-dir=/home/cory/.dotfiles/.git --work-tree=/home/cory/.dotfiles'
-	git --git-dir=/home/cory/.dotfiles/.git --work-tree=/home/cory/.dotfiles $argv;
+	git --git-dir=$DOTSDIR/.git --work-tree=$DOTSDIR $argv;
 end
